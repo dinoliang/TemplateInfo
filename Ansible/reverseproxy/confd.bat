38f1045d1ee85041808608612a85f43d3319f0ca
dinoliang <dinoliang@jsun.com>
2021-08-26 14:32:17 +0800

    modify gitignore

--- Ansible/reverseproxy/confd.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-setlocal enabledelayedexpansion
-
-START /min /i D:\reverseproxy\execute\confd.exe -confdir="D:\reverseproxy\execute\confd" -interval 10 -backend="etcdv3" -node="http://localhost:2379" -log-level="debug"
-
-endlocal
--- Ansible/reverseproxy/etcd.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-setlocal enabledelayedexpansion
-
-START /min /i D:\reverseproxy\execute\etcd\etcd.exe --name=etcd_node1 --listen-client-urls=http://0.0.0.0:2379 --advertise-client-urls=http://0.0.0.0:2379 --data-dir="D:\reverseproxy\execute\etcd\data"
-
-endlocal
